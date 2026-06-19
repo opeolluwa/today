0fe641df5b89de75d3788998c17b06ddcf4c7498
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2026-06-19 02:16:11 +0100

    chore: reusable UI lib

--- uilib/src/index.ts
+++
@@ -1,3 +0,0 @@
-export {default as BaseButton} from './components/forms/BaseButton.vue';
-export {default as BaseInput} from './components/forms/BaseInput.vue';
-export {default as BaseSelect} from './components/forms/BaseSelect.vue';
