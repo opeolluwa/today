fde8ae1cd84cd972d37992a3a3f0ceb3e1ef8282
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2026-06-19 02:15:42 +0100

    chore

---
+++ server/app/app/app.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import BaseInput from "../../../uilib/components/forms/BaseInput.vue";
+const inputValue = ref("");
+</script>
+
+<template>
+  <div>
+    <BaseInput
+      v-model="inputValue"
+      placeholder="Enter text"
+      label="Title"
+      name="bookmark title"
+    />
+    <h1>Welcome to the App</h1>
+    <p>This is the main application component.</p>
+  </div>
+</template>
